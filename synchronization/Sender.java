62e11902bdf011d8ce8fdfdb829f07ccfa95ff55
Aswinkkumar <aswinajce@gmail.com>
2022-02-03 02:08:20 +0530

    synchronization

---
+++ synchronization/Sender.java
@@ -0,0 +1,17 @@
+package com.aswin.synchronization;
+
+public class Sender {
+
+	void send(String msg)
+	{
+		System.out.println("Sending message "+msg);
+		try {
+			Thread.sleep(1000);
+
+		} catch (Exception e) {
+			System.out.println("Error while sending a message");
+		}
+		System.out.println("Message Sent successfully");
+	}
+}
+

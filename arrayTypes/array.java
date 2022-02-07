fba595c98159799cdb1c91f6b830e46e0b08698c
Aswinkkumar <aswinajce@gmail.com>
2022-02-07 16:46:07 +0530

    Array types

---
+++ arrayTypes/array.java
@@ -0,0 +1,21 @@
+package com.aswin.arrayTypes;
+
+public class array {
+
+
+		public static void main(String[] args) {
+
+		int a[]= {10,20,30,40,50};
+		for(int i=0;i<5;i++) {
+		System.out.println("Single dimensional Elements of array a: "+a[i]);
+		}
+		int[][] b = {
+		            {2, 4, 6, 8}, 
+		            {3, 6, 9} };
+		      
+		      System.out.println("\n multidimensional array Length of row 1: " + b[0].length);
+		      }
+		}
+
+
+

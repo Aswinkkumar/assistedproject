b7edd03dd582e43dceab808a1aa6a45bdfbb7324
Aswinkkumar <aswinajce@gmail.com>
2022-03-08 16:12:01 +0530

    Spring Security with Authentication .

---
+++ SpringSecurity-1/src/main/java/com/simplilearn/User.java
@@ -0,0 +1,46 @@
+package com.simplilearn;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String name;
+	private String password;
+	private String role;
+	
+	public User(String name, String password, String role) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.role = role;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
